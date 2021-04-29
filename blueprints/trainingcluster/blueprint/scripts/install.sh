f59aa69069d62f7ce2441e1eee15192373ecdbb4
Christine Gfrerer <cgfrerer@cs.sbg.ac.at>
2021-04-29 09:18:22 +0200

    prevent duplicates

--- blueprints/trainingcluster/blueprint/scripts/install.sh
+++ blueprints/trainingcluster/blueprint/scripts/install.sh
@@ -12,10 +12,9 @@ param_samples=$9
 param_epsilon=${10}
 sim_features=${11}
 sim_sources=${12}
-sim_samples=${13} 
+sim_samples=${13}
 
-
-# DYNAMIC VARIABLES
+# dynamic variable
 cd $CURRENT_WORKDIR
 
 git clone -b $git_branch $git_repo src
@@ -23,44 +22,43 @@ git clone -b $git_branch $git_repo src
 mkdir input
 mkdir output
 
-wget $graph_url -P input
+#download files
+wget -N $graph_url -P input
 
-if [ -z "$source_map" ]
+if [ -z "$source_map" ] || [ "$source_map" = "default" ]
     then
         echo "no source map as input"
     else
-        wget $source_map -P input
+        wget -N $source_map -P input
 fi
 
-if [ -z "$stats_url" ]
+if [ -z "$stats_url" ] || [ "$stats_url" = "default" ]
     then
         echo "no stats file as input"
     else
-        wget $stats_url -P input
+        wget -N $stats_url -P input
 fi
 
 
 #write config file for execution
-
 configfile="config.txt"
-touch $configfile
-echo "GIT_REPO=$git_repo" >> $configfile
-echo "GIT_BRANCH=$git_branch" >> $configfile
-echo "TOPIC=$topic" >> $configfile
-echo "JOB_ID=$job_id" >> $configfile
-echo "TASKS_PER_NODE=$tasks_per_node" >> $configfile
-echo "CURRENT_WORKDIR=$CURRENT_WORKDIR" >> $configfile
-echo "GRAPH_URL=$graph_url" >> $configfile
-echo "SOURCE_MAP_URL=$source_map" >> $configfile
-echo "STATS_URL=$stats_url" >> $configfile
-echo "PARAM_SAMPLES=$param_samples" >> $configfile
-echo "PARAM_EPSILON=$param_epsilon" >> $configfile
-echo "SIM_FEATURES=$sim_features" >> $configfile
-echo "SIM_SOURCES=$sim_sources" >> $configfile
-echo "SIM_SAMPLES=$sim_samples" >> $configfile
-
-#tmpfile="tmp.txt"
-#touch $tmpfile
-#env >> $tmpfile
-
-
+if [ -f "$configfile" ]
+    then
+        echo "config is already present"
+    else
+        touch $configfile
+        echo "GIT_REPO=$git_repo" >> $configfile
+        echo "GIT_BRANCH=$git_branch" >> $configfile
+        echo "TOPIC=$topic" >> $configfile
+        echo "JOB_ID=$job_id" >> $configfile
+        echo "TASKS_PER_NODE=$tasks_per_node" >> $configfile
+        echo "CURRENT_WORKDIR=$CURRENT_WORKDIR" >> $configfile
+        echo "GRAPH_URL=$graph_url" >> $configfile
+        echo "SOURCE_MAP_URL=$source_map" >> $configfile
+        echo "STATS_URL=$stats_url" >> $configfile
+        echo "PARAM_SAMPLES=$param_samples" >> $configfile
+        echo "PARAM_EPSILON=$param_epsilon" >> $configfile
+        echo "SIM_FEATURES=$sim_features" >> $configfile
+        echo "SIM_SOURCES=$sim_sources" >> $configfile
+        echo "SIM_SAMPLES=$sim_samples" >> $configfile
+fi
